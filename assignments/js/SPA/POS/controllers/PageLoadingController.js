56a7b80197775919482cc846aa105e1baa3fdab9
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2022-10-14 16:36:06 +0530

    added other page controller

--- assignments/js/SPA/POS/controllers/PageLoadingController.js
+++ assignments/js/SPA/POS/controllers/PageLoadingController.js
@@ -89,29 +89,3 @@ $('#othrbtttn,#linkOrderDetails2').click(function () {
 });
 // End When order click other section hide
 
-//set Save Customers Count in dashboard
-function setCusCount(){
-    let cusCount=0;
-    for (let customer of customers) {
-        cusCount=cusCount+1;
-    }
-    $('#customersCount').text(cusCount);
-}
-
-//set Save Items Count in dashboard
-function setItemsCount(){
-    let itemsCount=0;
-    for (let item of items) {
-        itemsCount=itemsCount+1;
-    }
-    $('#itemsCount').text(itemsCount);
-}
-
-//set Place Orders Count in dashboard
-function setOrdersCount(){
-    let ordersCount=0;
-    for (let orde of orderDetails) {
-        ordersCount=ordersCount+1;
-    }
-    $('#ordersCount').text(ordersCount);
-}
