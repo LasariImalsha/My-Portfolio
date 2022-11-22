00e2e6b6275b6939b0672c4cdbbb97b09582070e
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2022-11-22 13:16:04 +0530

    Added functions to ceate board

---
+++ assignments/js/Game/assets/js/script.js
@@ -0,0 +1,26 @@
+const cvs = document.getElementById("frame");
+const ctx = cvs.getContext("2d");
+
+const ROW = 20;
+const COL = 10;
+const SQ  = 20;//squareSize
+const VACANT = "#F0EBCE";
+
+let board = [];
+
+function drawSquare(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x*SQ,y*SQ,SQ,SQ);
+
+    ctx.strokeStyle = "#F0EBCE";
+    ctx.strokeRect(x*SQ,y*SQ,SQ,SQ);
+}
+
+for (r=0; r < ROW; r++ ){
+    board[r] = [];
+    for(c=0; c < COL; c++){
+        board[r,c] = VACANT;
+    }
+}
+
+drawSquare();
